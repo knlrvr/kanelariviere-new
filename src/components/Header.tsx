ad909e2502dcc77d5a2dcda4f51e57836a4f920a
Kane <kanelariviere@outlook.com>
2023-09-12 17:20:33 -0400

    first

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,7 +49,7 @@ export const Header = () => {
                                 />
                             )}
                             {item.label}
-                        </Link>
+                        </Link> 
                     </li>
                 ))}
             </ul>
