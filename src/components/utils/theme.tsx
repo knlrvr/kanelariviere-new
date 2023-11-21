cfb7d42cb2d6ca8935828b09c75d790c2df11fc1
Kane <kanelariviere@outlook.com>
2023-11-21 11:10:18 -0500

    refactor: optimize theme

--- src/components/utils/theme.tsx
+++ src/components/utils/theme.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 
 const useDarkMode = () => {
-  const getCurrentTheme = () => {
+  const getCurrentTheme = useMemo(() => {
     const currentHour = new Date().getHours();
     return currentHour < 6 || currentHour >= 17 ? 'dark' : 'light';
-  };
+  }, []);
 
   const [theme, setTheme] = useState(getCurrentTheme);
 
@@ -16,20 +16,12 @@ const useDarkMode = () => {
 
   useEffect(() => {
     const root = window.document.documentElement;
-    root.classList.remove(theme === 'dark' ? 'light' : 'dark');
-    root.classList.add(theme);
+    root.classList.toggle('light', theme === 'light');
+    root.classList.toggle('dark', theme === 'dark');
   }, [theme]);
-
-  // useEffect(() => {
-  //   // Update theme based on the current time every minute
-  //   const updateThemeInterval = setInterval(() => {
-  //     setTheme(getCurrentTheme());
-  //   }, 60000);
-
-  //   return () => clearInterval(updateThemeInterval);
-  // }, []);
 
   return [theme, toggleTheme] as const;
 };
 
 export default useDarkMode;
+
