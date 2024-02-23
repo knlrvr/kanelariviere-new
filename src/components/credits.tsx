abfd022aec4ae1dd8b0f31326129129a4f007ab9
Kane <kanelariviere@outlook.com>
2024-02-22 20:33:21 -0500

    CREDITS card not like a credit card lol

--- src/components/credits.tsx
+++ src/components/credits.tsx
@@ -23,13 +23,13 @@ export function Credits() {
         <div className="flex justify-between space-x-4">
           <div className="space-y-1 text-xs text-neutral-500">
             <div className="flex items-center gap-2">
-              <FiSmile />
+              <FiSmile /> 
               <span>Built by&nbsp;
                 <Link href="https://github.com/knlrvr" target="_blank" className="inline-flex underline underline-offset-2 hover:text-[var(--text)] transition-colors duration-200">Kane Lariviere</Link>.
               </span>
             </div>
             <div className="flex items-center gap-2">
-              <FiCode />
+              <FiCode /> 
               <span>Built with&nbsp;
                 <Link href="https://nextjs.org/" target="_blank" className="inline-flex underline underline-offset-2 hover:text-[var(--text)] transition-colors duration-200">Next.js</Link>,&nbsp;
                 <Link href="https://tailwindcss.com/" target="_blank" className="inline-flex underline underline-offset-2 hover:text-[var(--text)] transition-colors duration-200">Tailwind</Link>,&nbsp;&amp;&nbsp;
@@ -37,13 +37,13 @@ export function Credits() {
               </span>
             </div>
             <div className="flex items-center gap-2">
-              <FiGithub />
+              <FiGithub /> 
               <span>Source code available on&nbsp;
                 <Link href="https://github.com/knlrvr/kanelariviere-new" target="_blank" className="inline-flex underline underline-offset-2 hover:text-[var(--text)] transition-colors duration-200">GitHub</Link>.
               </span>
             </div>
             <div className="flex items-center gap-2">
-              <FiTriangle />
+              <FiTriangle /> 
               <span>Hosted on&nbsp;
                 <Link href="https://vercel.com" target="_blank" className="inline-flex underline underline-offset-2 hover:text-[var(--text)] transition-colors duration-200">Vercel</Link>.
               </span> 
